01f1fd2bdd851b6f14fabf4ac0a9f8465af8481c
YuJin Kwon <tokyj515@naver.com>
2023-06-22 18:11:18 +0900

    fix: 주석, 이름 변경 #6

--- src/main/java/com/semtleapp/semtleapp/semtleuser/config/SecurityConfig.java
+++ src/main/java/com/semtleapp/semtleapp/semtleuser/config/SecurityConfig.java
@@ -28,7 +28,7 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig {
+public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final AuthenticationEntryPointHandler authenticationEntryPointHandler;
