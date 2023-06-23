74ce02eb9e560c5ed263a9f5c7cb59a5d99d446f
YuJin Kwon <tokyj515@naver.com>
2023-06-23 20:57:31 +0900

    [Feat/#6] DI 어노테이션 변경

--- src/main/java/com/semtleapp/semtleapp/semtlepost/controller/SemtlePostController.java
+++ src/main/java/com/semtleapp/semtleapp/semtlepost/controller/SemtlePostController.java
@@ -4,6 +4,8 @@ import com.semtleapp.semtleapp.semtlepost.dto.SemtlePostDto;
 import com.semtleapp.semtleapp.semtlepost.service.SemtlePostService;
 import com.semtleapp.semtleapp.semtlepost.service.SemtlePostServiceImpl;
 import com.semtleapp.semtleapp.global.entity.CustomResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,15 +13,13 @@ import java.security.Principal;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/post")
 public class SemtlePostController {
     private final SemtlePostService semtlePostService;
 
-    public SemtlePostController(SemtlePostServiceImpl semtlePostService) {
-        this.semtlePostService = semtlePostService;
-    }
-
+    @Tag(name = "post", description = "공지사항 API")
     @PostMapping("/write")
     public CustomResponse createPost(Principal principal, @RequestPart(value = "image", required = false) List<MultipartFile> files, @RequestPart(value = "semtlePostDto") SemtlePostDto semtlePostDto) throws Exception {
         //principal.getName() 하면 시큐리티에서 들어온 회원의 이매일을 가지고옴.
--- src/main/java/com/semtleapp/semtleapp/semtleuser/controller/SemtleUserController.java
+++ src/main/java/com/semtleapp/semtleapp/semtleuser/controller/SemtleUserController.java
@@ -5,23 +5,20 @@ import com.semtleapp.semtleapp.semtleuser.dto.SemtleUserDto;
 import com.semtleapp.semtleapp.global.jwt.JwtService;
 import com.semtleapp.semtleapp.semtleuser.service.SemtleUserService;
 import com.semtleapp.semtleapp.semtleuser.service.SemtleUserServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class SemtleUserController {
     private final SemtleUserService semtleUserService;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
-
-    public SemtleUserController(SemtleUserServiceImpl memberService, PasswordEncoder passwordEncoder, JwtService jwtService) {
-        this.semtleUserService = memberService;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
-    }
 
     @PostMapping("/signup")
     public CustomResponse signup(@RequestBody SemtleUserDto semtleUserDto) {
