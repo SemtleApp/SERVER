c1037b81b3f15ee427c0a469fb250250adf25a91
YuJin Kwon <tokyj515@naver.com>
2023-06-26 10:28:33 +0900

    [Feat/#14] testController swagger 주석 추가

--- src/main/java/com/semtleapp/semtleapp/global/test/testController.java
+++ src/main/java/com/semtleapp/semtleapp/global/test/testController.java
@@ -3,6 +3,8 @@ package com.semtleapp.semtleapp.global.test;
 
 import com.semtleapp.semtleapp.global.entity.ApiResponse;
 import com.semtleapp.semtleapp.global.exception.CustomException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import static com.semtleapp.semtleapp.global.exception.ErrorCode.BAD_REQUEST;
 import static com.semtleapp.semtleapp.global.exception.ErrorCode.USERNAME_OR_PASSWORD_NOT_FOUND_EXCEPTION;
 
 @RequiredArgsConstructor
+@Api(tags={"00. Controller 예시"})
 @RequestMapping("/test")
 @RestController
 public class testController {
@@ -27,12 +30,12 @@ public class testController {
 //        throw new CustomException(BAD_REQUEST);
 //    }
 
+    @ApiOperation(value = "컨트롤러 예시", notes = "null 입력하면 400오류 발생, 입력하지 않으면 200 성공")
     @GetMapping("")
     public ApiResponse<String> test(@RequestParam(required = false) String s){
 
         if("null".equals(s))
             throw new CustomException(BAD_REQUEST);
-
 
         return new ApiResponse<>("test 성공");
     }
