149dade7ddc601699960e8d403520fb526c49727
wlsdbsrb <diejeadong@naver.com>
2023-07-09 22:34:12 +0900

    [Feat/#17] 스터디글 작성

--- src/main/java/com/semtleapp/semtleapp/semtleuser/dto/Role.java
+++ src/main/java/com/semtleapp/semtleapp/semtleuser/dto/Role.java
@@ -7,13 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Role {
-    ADMIN("ROLE_ADMIN"), //회장
-    ASEM("ROLE_ASEM"), //부회장
-    BSEM("ROLE_BSEM"), //총무
-    CSEM("ROLE_CSEM"), //서기
-    DSEM("ROLE_DSEM"), //홍보
-    ESEM("ROLE_ESEM"), //스터디
-    USER("ROLE_USER"); //일반 유저
+    ROLE_ADMIN("ROLE_ADMIN"), //회장
+    ROLE_ASEM("ROLE_ASEM"), //부회장
+    ROLE_BSEM("ROLE_BSEM"), //총무
+    ROLE_CSEM("ROLE_CSEM"), //서기
+    ROLE_DSEM("ROLE_DSEM"), //홍보
+    ROLE_ESEM("ROLE_ESEM"), //스터디
+    ROLE_USER("ROLE_USER"); //일반 유저
     private String value;
 
     @JsonCreator
