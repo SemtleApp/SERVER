d6d709b4fd30df89c904780b2dedf49acb2c54ac
YuJin Kwon <tokyj515@naver.com>
2023-07-10 14:55:16 +0900

    [Feat/#14] userInfo 테이블, 클래스 생성

---
+++ src/main/java/com/semtleapp/semtleapp/semtleuser/entity/SemtleUserInfo.java
@@ -0,0 +1,42 @@
+package com.semtleapp.semtleapp.semtleuser.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "semtle_user_info")
+public class SemtleUserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "info_id", unique = true, nullable = false)
+    private Long infoId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "grade")
+    private int grade;
+
+    @Column(name = "student_id")
+    private int studentId;
+
+    @Column(name = "phone")
+    private String phone;
+
+
+}
