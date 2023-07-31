dedae4a24f840deeb06ee31625775116a6356833
이다빈 <dlekqls6204@naver.com>
2023-07-31 21:22:28 +0900

    [Feat/#34] 공지사항 게시글 삭제

--- src/main/java/com/semtleapp/semtleapp/semtlenotice/service/SemtleNoticeService.java
+++ src/main/java/com/semtleapp/semtleapp/semtlenotice/service/SemtleNoticeService.java
@@ -9,5 +9,6 @@ import java.util.List;
 public interface SemtleNoticeService {
     SemtleNoticeRes.PostNoticeRes registerNoticePost(String email, SemtleNoticeReq.PostNoticeReq postNoticeReq, List<MultipartFile> files);
     SemtleNoticeRes.PostNoticeRes modifyNoticePost(String email, SemtleNoticeReq.PatchNoticeReq patchNoticeReq, List<MultipartFile> files);
+    SemtleNoticeRes.PostNoticeRes deleteNoticePost(String email, Long postId);
 
 }
