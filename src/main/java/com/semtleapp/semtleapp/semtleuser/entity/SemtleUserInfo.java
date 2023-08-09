01ce893125b1e7e054d7a79990b0d14b5e3530be
YuJin Kwon <tokyj515@naver.com>
2023-08-09 21:48:48 +0900

    [Feat/#14] status 컬럼 추가 + 디비에 컬럼 추가

--- src/main/java/com/semtleapp/semtleapp/semtleuser/entity/SemtleUserInfo.java
+++ src/main/java/com/semtleapp/semtleapp/semtleuser/entity/SemtleUserInfo.java
@@ -43,5 +43,8 @@ public class SemtleUserInfo {
     @Column(name = "phone")
     private String phone;
 
+    @Enumerated(value = EnumType.STRING) //enum의 값 자체를 저장
+    private String status;
+
 
 }
