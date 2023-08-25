0823e9b1916c47340eb8a7db5ad6caaa74d7dee8
YuJin Kwon <tokyj515@naver.com>
2023-08-25 20:47:21 +0900

    [Feat/#14] 오류 멘트 수정

--- src/main/java/com/semtleapp/semtleapp/global/exception/ErrorCode.java
+++ src/main/java/com/semtleapp/semtleapp/global/exception/ErrorCode.java
@@ -32,7 +32,7 @@ public enum ErrorCode {
 
     UNAUTHORIZED_MEMBER(400, "관리자의 승인이 나지 않았습니다", HttpStatus.BAD_REQUEST),
 
-    NOT_EXIST_USER(400, "존재하지 않는 유저입니다", HttpStatus.BAD_REQUEST),
+    NOT_EXIST_USER(400, "존재하지 않는 아이디입니다", HttpStatus.BAD_REQUEST),
 
     DUPLICATE_BOOK(400,"이미 등록된 도서입니다.",HttpStatus.BAD_REQUEST),
     NOT_EXIST_BOOK(400,"존재하지 않는 도서입니다.",HttpStatus.NOT_FOUND),
